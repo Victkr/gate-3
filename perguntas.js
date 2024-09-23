d3ebbbb7c63d3f5d26cf4078c2c88dc11d1c4244
Victkr <165177894+Victkr@users.noreply.github.com>
2024-09-22 22:23:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'jogos',
+    'Qual jogo ganhou a game awards de 2024?',
+    'O famoso Baldurs Gate 3'
+)
+
+criaCartao(
+    'esporte',
+    'Qual é o esporte mais praticado no mundo?',
+    'o esporte mais praticado é o Futebol'
+)
+
+criaCartao(
+    'jogos',
+    'Qual é o jogo mais caro do mundo?',
+    'O jogo mais caro do mundo é The Hidden and Unknown'
+)
+
+criaCartao(
+    'francês',
+    'Como se diz boa tarde em francês?',
+    'bon après-midi'
+)
